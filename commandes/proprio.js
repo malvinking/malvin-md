98c964969daf868df16ee96de721cfbac2429bf7
Malvin King <163745886+malvinking@users.noreply.github.com>
2024-06-24 15:58:52 +0200

    Add files via upload

---
+++ commandes/proprio.js
@@ -0,0 +1,31 @@
+const {zokou}=require("../framework/zokou")
+
+
+
+
+
+
+
+zokou({nomCom:"reboot",categorie:"Mods",reaction:"👨🏿‍💼"},async(dest,z,com)=>{
+
+
+  
+const{repondre,ms,dev,superUser}=com;
+
+  if(!superUser)
+  {
+    return repondre("This command is for *ᴍᴀʟᴠɪɴ-ᴍᴅ* owner only");
+  }
+
+  const {exec}=require("child_process")
+
+    repondre("*restarting ...*");
+
+  exec("pm2 restart all");
+  
+
+  
+
+
+
+})
